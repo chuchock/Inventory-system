7e462edaffcc04ca216ddf49ab948cecd64bc348
Jesús Montero <jesusmontero.developer@gmail.com>
2020-07-24 12:26:28 -0500

    Added InOut storage functionality

--- Business/B_Storage.cs
+++ Business/B_Storage.cs
@@ -4,11 +4,32 @@ using System.Linq;
 using System.Text;
 using DataAccess;
 using Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Business
 {
 	public class B_Storage
 	{
+		public static List<StorageEntity> StorageList()
+		{
+			using (var db = new InventaryContext())
+			{
+				return db.Storages.ToList();
+			}
+		}
+
+		public static List<StorageEntity> StorageProductsByWarehouse(string idWarehouse)
+		{
+			using (var db = new InventaryContext())
+			{
+				return db.Storages
+					.Include(s => s.Product)
+					.Include(s => s.Warehouse)
+					.Where(s => s.WarehouseId == idWarehouse)
+					.ToList();
+			}
+		}
+
 		public static void CreateStorage(StorageEntity oStorage)
 		{
 			using (var db = new InventaryContext())
@@ -26,7 +47,15 @@ namespace Business
 					.Where(s => s.StorageId == idStorage);
 
 				return product.Any();
+			}
+		}
 
+		public static void UpdateStorage(StorageEntity oStorage)
+		{
+			using (var db = new InventaryContext())
+			{
+				db.Storages.Update(oStorage);
+				db.SaveChanges();
 			}
 		}
 	}
